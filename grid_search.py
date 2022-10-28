e8457776b8147c8061f289d06731b0e283913a9b
yweizhang <zhang2486@wisc.edu>
2022-10-28 16:58:40 -0500

    f

--- grid_search.py
+++ grid_search.py
@@ -264,9 +264,9 @@ if __name__ == "__main__":
     parser.add_argument(
         "--batch-size", type=int, help="batch size"
     )
-    parser.add_argument(
-        "--config-file", type=str, help="config file"
-    )
+    # parser.add_argument(
+    #     "--config-file", type=str, help="config file"
+    # )
     parser.add_argument(
         "--dataset-config-file", type=str, help="dataset config file"
     )
