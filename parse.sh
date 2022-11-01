276244d9bc95871da6efbca099289c1971f13515
yweizhang <zhang2486@wisc.edu>
2022-11-01 09:51:47 -0500

    f

--- parse.sh
+++ parse.sh
@@ -3,6 +3,6 @@ CATEGORY=$2
 DATASET=$3
 CFG=test
 SHOTS=16
-
+TRAINER=IVLP
 
 python parse_test_res.py /nobackup3/yiwei/output_ivlp/${OUTPUT}/base2new/${CATEGORY}/${DATASET}/shots_${SHOTS}/${TRAINER}/${CFG}/ --test-log
