373aa2f8d49f5ea41d52872c89203f3c70d9f1b8
yweizhang <zhang2486@wisc.edu>
2022-11-01 09:49:55 -0500

    f

--- parse.sh
+++ parse.sh
@@ -5,4 +5,4 @@ CFG=vit_b16_c2_ep10_batch8_4+4ctx
 SHOTS=16
 
 
-python parse_test_res.py /nobackup3/yiwei/output_ivlp/${OUTPUT}/base2new/train_base/${DATASET}/shots_${SHOTS}/${TRAINER}/${CFG}/ --test-log
+python parse_test_res.py /nobackup3/yiwei/output_ivlp/${OUTPUT}/base2new/${CATEGORY}/${DATASET}/shots_${SHOTS}/${TRAINER}/${CFG}/ --test-log
